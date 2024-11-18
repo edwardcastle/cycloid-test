7c0d35ca8e28bf077ab7c17802cd21e9c7ab4c3d
_edwardcastle <ecastle@elkanodata.com>
2024-11-18 18:12:49 +0100

    feat: create function for getting all the fruits from api

--- src/stores/fruits.js
+++ src/stores/fruits.js
@@ -1,3 +1,53 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useFruits = defineStore('fruits', () => {})
+export const useFruits = defineStore('fruits', () => {
+  const BASE_URL = import.meta.env.VITE_API_URL
+  const fruits = ref([])
+  const fruitsLength = ref(0);
+
+  /**
+   * Given a complex json return an array where contain {isFruit}
+   * @param data{Object}
+   * @return void
+   * */
+  const extractFruits = data => {
+    const explore = node => {
+      if (typeof node === 'object' && node !== null) {
+        if (node.isFruit) {
+          fruits.value.push(node)
+        } else {
+          for (const key in node) {
+            explore(node[key])
+          }
+        }
+      }
+    }
+    explore(data)
+  }
+
+  /**
+   * Get all the objects from api
+   * @return void
+   */
+  const getFruits = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/fruit`)
+      if (!response.ok) {
+        throw new Error(`${response?.statusText}`)
+      } else {
+        const result = await response.json()
+        fruitsLength.value = result.data.fruitCount
+        extractFruits(result.data)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return {
+    fruits,
+    fruitsLength,
+    getFruits,
+  }
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { onMounted } from 'vue'
+import { useFruits } from '@/stores/fruits.js'
+
+const store = useFruits();
+onMounted(async () => await store.getFruits())
 </script>
 
 <template>
   <main>
+    {{ store.fruits }}
   </main>
 </template>
